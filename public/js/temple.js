7f1c9bccf4bba5e28f332d94d4d603a0aa3b7a70
matiasmunives <mun19005@byui.edu>
2024-07-25 01:25:53 -0300

    final-project

--- public/js/temple.js
+++ public/js/temple.js
@@ -22,26 +22,35 @@
   }) 
  })
 
- // Build inventory items into HTML table components and inject into DOM 
+   // Build temple items into HTML table components and inject into DOM
 function buildTempleList(data) { 
     let templeDisplay = document.getElementById("templeDisplay"); 
     // Set up the table labels 
-    let dataTable = '<thead>'; 
-    dataTable += '<tr><th>Temple Name</th><td>&nbsp;</td><td>&nbsp;</td></tr>'; 
+    let dataTable = '<thead>';
+    dataTable += '<tr><th>ID</th><th>Temple</th><th>Address</th><th>City</th><th>Country</th><th>Phone Number</th><th>G-Date</th><th>D-Date</th><th colspan="3">Actions</th></tr>'; 
     dataTable += '</thead>'; 
     // Set up the table body 
     dataTable += '<tbody>'; 
-    // Iterate over all vehicles in the array and put each in a row 
+    // Iterate over all temples in the array and put each in a row 
     data.forEach(function (element) { 
      console.log(element.temp_id + ", " + element.temp_name); 
-     dataTable += `<tr><td>${element.temp_picture} ${element.temp_name} ${element.temp_address} ${element.temp_city} ${element.temp_country} ${element.temp_phone} ${element.temp_address} ${element.temp_bdate} ${element.temp_ddate}</td>`; 
-     dataTable += `<td><a href='/temp/view/${element.temp_id}' title='Click to delete'>View</a></td></tr>`; 
+     
+     let formattedBdate = moment(element.temp_bdate).format('MM-DD-YYYY');
+     let formattedDdate = moment(element.temp_ddate).format('MM-DD-YYYY');
+     
+     dataTable += `<td>${element.temp_id}</td>`;
+     dataTable += `<td>${element.temp_name}</td>`;
+     dataTable += `<td>${element.temp_address}</td>`;
+     dataTable += `<td>${element.temp_city}</td>`;
+     dataTable += `<td>${element.temp_country}</td>`;
+     dataTable += `<td>${element.temp_phone}</td>`;
+     dataTable += `<td>${formattedBdate}</td>`;
+     dataTable += `<td>${formattedDdate}</td>`;
+     dataTable += `<td><a href='/temp/view/${element.temp_id}' title='Click to delete'>View</a></td>`; 
      dataTable += `<td><a href='/temp/edit/${element.temp_id}' title='Click to update'>Modify</a></td>`; 
      dataTable += `<td><a href='/temp/delete/${element.temp_id}' title='Click to delete'>Delete</a></td></tr>`; 
-     dataTable += `<td><a href='/temp/add/${element.temp_id}' title='Click to delete'>Add</a></td></tr>`; 
-     dataTable += `<td><a href='/temp/view/${element.temp_id}' title='Click to delete'>View</a></td></tr>`; 
-    }) 
+    }); 
     dataTable += '</tbody>'; 
     // Display the contents in the Temple Management view 
     templeDisplay.innerHTML = dataTable; 
-   }
+}
